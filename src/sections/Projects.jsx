13672737dbfc37f8e95ba109daff592138c3ed27
Elias Pedevilla <pedevillae@gmail.com>
2022-11-24 02:28:44 -0300

    update projects information

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className="w-full flex flex-col items-center" id="projects">
       <h2 className=" text-3xl font-semibold pt-9 ">Proyectos</h2>
-      <div className=" grid grid-cols-3 gap-11 pt-11 ">
+      <div className=" grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2  gap-11 pt-11 ">
         {data.projects.map((project) => {
           return (
             <ProjectCard
